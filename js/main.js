346b70e450c7deeae3bff35a9550f80ce06a13eb
Artemy Marchenko <artemy.marchenko2000@gmail.com>
2024-06-10 13:52:38 +0300

    TOCHNO ZOLOTO

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ document.oncontextmenu = function() { return false; }
 document.addEventListener('DOMContentLoaded', function () {
     const sliderData = [
       { 
-        coverSrc: 'images/pine.png', 
+        coverSrc: 'images/Pine_cover.png', 
         coverDescription: 'Pinery, кофе- и какао-бар',
         year:'2023',
         images: [ // Массив изображений, связанных с обложкой 1
-        { src: 'images/Pine1.png', description: 'Проект по созданию визуального стиля для сети московских кофеен Пайнери, в том числе интерфейсов для сайта и фирменной одежды для баристаВ роли: артдира, тимлида, веб, графика', link: 'https://google.com'  },
-        { src: 'images/Pine2.png', description: '' },
-        { src: 'images/Pine3.png', description: '' },
-        { src: 'images/Pine4.png', description: '' },
-        { src: 'images/Pine5.png', description: '' },
-        { src: 'images/Pine6.png', description: '' },
+        { src: 'images/Pine1.png', description: 'Проект по созданию визуального стиля для сети московских кофеен Пайнери, в том числе интерфейсов для сайта и фирменной одежды для бариста В роли: артдира, тимлида, веб, графика', link: 'https://pinerycacao.ru/'  },
+        { src: 'images/Pine2.png', description: '', link: 'https://pinerycacao.ru/' },
+        { src: 'images/Pine3.png', description: '', link: 'https://pinerycacao.ru/' },
+        { src: 'images/Pine4.png', description: '', link: 'https://pinerycacao.ru/' },
+        { src: 'images/Pine5.png', description: '', link: 'https://pinerycacao.ru/' },
+        { src: 'images/Pine6.png', description: '', link: 'https://pinerycacao.ru/' },
 
         ]
       },
@@ -39,22 +39,22 @@ document.addEventListener('DOMContentLoaded', function () {
         coverDescription: 'Mozabrick бренд',
         year:'2022',
         images: [ 
-          { src: 'images/mozabrick1.jpg', description: 'Ведение брендов Mozabrick, RingString и QBRIX. Разработка полиграфии, упаковки и сайтовВ роли: тимлида, веб, упаковки, графика' },
-          { src: 'images/mozabrick2.jpeg', description: '' },
-          { src: 'images/mozabrick3.jpeg', description: '' },
-          { src: 'images/mozabrick3.png', description: '' },
-          { src: 'images/mozabrick4.png', description: '' },
-          { src: 'images/mozabrick5.png', description: '' },
-          { src: 'images/mozabrick6.png', description: '' },
-          { src: 'images/mozabrick7.jpg', description: '' },
-          { src: 'images/mozabrick8.jpg', description: '' },
-          { src: 'images/mozabrick9.png', description: '' },
-          { src: 'images/mozabrick10.png', description: '' },
-          { src: 'images/mozabrick11.png', description: '' },
-          { src: 'images/mozabrick12.gif', description: '' },
-          { src: 'images/mozabrick13.gif', description: '' },
-          { src: 'images/mozabrick14.png', description: '' },
-          { src: 'images/mozabrick15.png', description: '' },
+          { src: 'images/mozabrick1.jpg', description: 'Ведение брендов Mozabrick, RingString и QBRIX. Разработка полиграфии, упаковки и сайтов В роли: тимлида, веб, упаковки, графика', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick2.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick3.jpeg', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick3.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick4.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick5.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick6.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick7.jpg', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick8.jpg', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick9.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick10.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick11.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick12.gif', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick13.gif', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick14.png', description: '', link: "https://mozabrick.ru/" },
+          { src: 'images/mozabrick15.png', description: '', link: "https://mozabrick.ru/" },
         ]
       },
       { 
@@ -62,14 +62,14 @@ document.addEventListener('DOMContentLoaded', function () {
         coverDescription: 'Агентура Вариативная',
         year:'2023',
         images: [ 
-          { src: 'images/Agent1.jpg', description: 'Проект реформы русского алфавита на основе кириллицы. Сопровождён текстом научной статьи в системе РИНЦ В роли: автора' },
-          { src: 'images/Agent2.jpg', description: '' },
-          { src: 'images/Agent3.jpg', description: '' },
-          { src: 'images/Agent4.jpg', description: '' },
-          { src: 'images/Agent5.jpg', description: '' },
-          { src: 'images/Agent6.jpg', description: '' },
-          { src: 'images/Agent7.jpg', description: '' },
-          { src: 'images/Agent8.jpg', description: '' },
+          { src: 'images/Agent1.jpg', description: 'Проект реформы русского алфавита на основе кириллицы. Сопровождён текстом научной статьи в системе РИНЦ В роли: автора', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent2.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent3.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent4.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent5.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent6.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent7.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
+          { src: 'images/Agent8.jpg', description: '', link:'https://www.elibrary.ru/item.asp?id=54648815' },
 
         ]
       },
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function () {
         coverDescription: 'Закрытая',
         year:'2023',
         images: [ 
-          { src: 'images/Closed1.png', description: 'Шрифт с очень закрытым характером знаков на основе классического модернистского подхода. Этим шрифтом набран этот сайтВ роли: автора' },
+          { src: 'images/Closed1.png', description: 'Шрифт с очень закрытым характером знаков на основе классического модернистского подхода. Этим шрифтом набран этот сайт В роли: автора' },
           { src: 'images/Closed2.png', description: '' },
           { src: 'images/Closed3.png', description: '' },
 
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function () {
         coverDescription: 'Кино Центральный',
         year:'2024',
         images: [ 
-          { src: 'images/Kino1.png', description: 'Экспериментальный гротеск с акцентом на геометрии знаков. Содержит авангардную кассу кириллицы и некоторые спецсимволыВ роли: автора' },
+          { src: 'images/Kino1.png', description: 'Экспериментальный гротеск с акцентом на геометрии знаков. Содержит авангардную кассу кириллицы и некоторые спецсимволы В роли: автора' },
           { src: 'images/Kino2.png', description: '' },
           { src: 'images/Kino3.png', description: '' },
           { src: 'images/Kino4.png', description: '' },
@@ -128,35 +128,39 @@ document.addEventListener('DOMContentLoaded', function () {
         coverDescription: 'Мэзон Книжная',
         year:'2024',
         images: [ 
-          { src: 'images/Book1.png', description: 'Антиква, целью которой стала систематизация пропорций знаков прежде всего кирилловского письма на поле шрифтов с засечкамиВ роли: автора' },
-          { src: 'images/Book2.png', description: '' },
+          { src: 'images/Book1.png', description: 'Антиква, целью которой стала систематизация пропорций знаков прежде всего кирилловского письма на поле шрифтов с засечками В роли: автора' },
+          { src: 'images/Book8.png', description: '' },
+          { src: 'images/Book9.png', description: '' },
           { src: 'images/Book3.png', description: '' },
           { src: 'images/Book4.png', description: '' },
           { src: 'images/Book5.png', description: '' },
           { src: 'images/Book6.png', description: '' },
+          { src: 'images/Book7.png', description: '' },
         ]
       },
       { 
         coverSrc: 'images/Handsome_cover.png', // Путь к обложке 2
         coverDescription: 'Красавицы могут все',
         year:'2023',
+        nda: true,
         images: [ 
-          { src: 'images/Handsome1.png', description: 'Разработка фирменного стиля, тары и упаковки для люкс-бренда косметики. NDA, по запросуВ роли: артдира, промграфика, упаковка' },
-          { src: 'images/Handsome2.png', description: '' },
-          { src: 'images/Handsome3.png', description: '' },
-          { src: 'images/Handsome4.png', description: '' },
-          { src: 'images/Handsome5.png', description: '' },
+          { src: 'images/Handsome1.png', description: 'Разработка фирменного стиля, тары и упаковки для люкс-бренда косметики. NDA, по запросу В роли: артдира, промграфика, упаковка',  nda: true },
+          { src: 'images/Handsome2.png', description: '', nda: true },
+          { src: 'images/Handsome3.png', description: '', nda: true },
+          { src: 'images/Handsome4.png', description: '', nda: true },
+          { src: 'images/Handsome5.png', description: '', nda: true },
          
         ]
       },
       { 
         coverSrc: 'images/School_cover.png', // Путь к обложке 2
-        coverDescription: 'Я школа',
+        coverDescription: 'Онлайн школа',
         year:'2023',
+        nda: true,
         images: [ 
-          { src: 'images/school1.png', description: 'Проект по созданию онлайн-школы и интерфейса для нее в рамках учебного заведения. Проведение анализа и исследований с участием учеников, учителей, а так же непосредственно проектная деятельность В роли: продакта, артдира, тимлида' },
-          { src: 'images/school2.png', description: '' },
-          { src: 'images/school3.png', description: '' },
+          { src: 'images/school1.png', description: 'Проект по созданию онлайн-школы и интерфейса для нее в рамках учебного заведения. Проведение анализа и исследований с участием учеников, учителей, а так же непосредственно проектная деятельность В роли: продакта, артдира, тимлида', link:'https://living-crop-208.notion.site/d30abe72d8674f1eb299f1a30ea1ab4c',  nda: true },
+          { src: 'images/school2.png', description: '', link:'https://living-crop-208.notion.site/d30abe72d8674f1eb299f1a30ea1ab4c',  nda: true },
+          { src: 'images/school3.png', description: '', link:'https://living-crop-208.notion.site/d30abe72d8674f1eb299f1a30ea1ab4c',  nda: true },
          
         ]
       },
@@ -189,13 +193,21 @@ document.addEventListener('DOMContentLoaded', function () {
           const imgElement = document.createElement('img');
           imgElement.src = coverData.coverSrc;
           imgElement.alt = coverData.coverDescription;
-          imgElement.style.display = imgIndex === 0 ? 'block' : 'none';
+          imgElement.style.display = imgIndex === 0 ? 'flex' : 'none';
           imgElement.addEventListener('click', () => openPopup(imgIndex));
-    
+      
           // Добавляем классы к каждой обложке
           imgElement.classList.add('slide');
           imgElement.classList.add(`cover-${index + 1}`);
-    
+          
+          // Добавляем класс 'nda' если свойство nda: true
+          if (coverData.nda) {
+            slider.classList.add('nda');
+          }
+          else {
+            slider.classList.remove('nda');
+          }
+      
           slider.appendChild(imgElement);
         });
         document.querySelector('.about_project h3').innerText = coverData.year;
@@ -208,7 +220,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const imageData = coverData.images[currentSlide];
         popupImg.src = imageData.src;
         popupText.innerText = imageData.description;
-    
+      
         if (imageData.link) {
           popupText.setAttribute('data-has-link', 'true');
           popupText.onclick = () => {
@@ -218,7 +230,13 @@ document.addEventListener('DOMContentLoaded', function () {
           popupText.setAttribute('data-has-link', 'false');
           popupText.onclick = null;
         }
-    
+      
+        if (coverData.nda) {
+          document.querySelector('.popup-content').classList.add('nda');
+        } else {
+          document.querySelector('.popup-content').classList.remove('nda');
+        }
+      
         popup.style.display = 'flex';
         popupOpen = true;
         clearInterval(timer);
@@ -300,4 +318,13 @@ button.addEventListener('mousemove', (e) => {
     y = e.offsetY;
     button.style.setProperty('--mouse-x', x + "px");
     button.style.setProperty('--mouse-y', y + "px");
+});
+document.querySelectorAll('.prev-btn, .next-btn').forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    var circle = this.querySelector('circle');
+    var svg = this.querySelector('svg');
+    var newCircle = circle.cloneNode(true);
+    svg.replaceChild(newCircle, circle);
+    newCircle.style.animation = 'fill 0.6s ease-out';
+  });
 });
